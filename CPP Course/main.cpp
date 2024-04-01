3fd5fda7c1960a7e3684c5970e2fb6c6cb6c4d22
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2024-04-01 16:50:07 -0300

    Initial Commit

---
+++ CPP Course/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CPP Course
+//
+//  Created by Anderson Sprenger on 01/04/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
